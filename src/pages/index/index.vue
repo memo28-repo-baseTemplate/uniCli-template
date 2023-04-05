3e20221a9b76709833472639e1b025535e355213
hooks_28 <1r56955415918@qq.com>
2023-04-05 17:44:12 +0800

    feat: init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,43 @@
+<!--
+ * @Author: 邱狮杰
+ * @Date: 2023-02-09 21:17:58
+ * @LastEditTime: 2023-04-05 17:32:23
+ * @Description: 
+ * @FilePath: /v3/src/pages/index/index.vue
+-->
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png" />
+    <view class="text-area"> </view>
+  </view>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
